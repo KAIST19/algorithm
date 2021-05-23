8f5639405d51c33e48fecda10015bfdaa6968c45
KAIST19 <55547145+KAIST19@users.noreply.github.com>
2021-05-23 15:28:26 +0900

    Create KMP.py

---
+++ KMP.py
@@ -0,0 +1,40 @@
+# Generate the Longest Proper Prefix list (aka LPS)
+def LPS_generator(pattern):
+    l_pattern = len(pattern)
+    LPS = [0] * l_pattern
+    i = 1
+    length = 0
+    while i < l_pattern:
+        if pattern[i] == pattern[length]:
+            length += 1
+            LPS[i] = length
+            i += 1
+        else:
+            if length > 0:
+                length = LPS[length - 1]
+            else:
+                LPS[i] = 0
+                i += 1
+    return LPS
+
+
+#  KMP: find the "pattern" in the "passage"
+#  Both should be string
+def KMP(pattern, passage):
+    LPS = LPS_generator(pattern)
+    i = 0
+    j = 0
+    l_passage = len(passage)
+    l_pattern = len(pattern)
+    while i < l_passage:
+        if passage[i] == pattern[j]:
+            i += 1
+            j += 1
+        else:
+            if j == 0:
+                i += 1
+            else:
+                j = LPS[j - 1]
+        if j == l_pattern:
+            yield i - j + 1 #  The index of the word
+            j = LPS[j - 1]
