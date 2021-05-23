26582cd0cb16214dc9d43d0bed042a7dc402edd9
Jackson Kim <kaist19@kaist.ac.kr>
2021-05-23 22:02:57 +0900

    Create Extended_Euclidean_Algorithm.py

---
+++ Number_Theory/Extended_Euclidean_Algorithm.py
@@ -0,0 +1,8 @@
+# The extended Euclidean algorithm
+# Returns the gcd of two integers and the solutions
+def EEA(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = EEA(b % a, a)
+        return gcd, y - (b // a) * x, x
