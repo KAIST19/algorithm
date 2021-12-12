b441232736e9c2f2ffaf72aed6a29eec573eacb0
KH Kim <kaist19@kaist.ac.kr>
2021-12-12 17:28:19 +0900

    java code reset

--- ADT_java/Tree/Tree.java
+++
@@ -1,9 +0,0 @@
-package Tree;
-
-public interface Tree<Key extends Comparable<Key>, Value> {
-    public Value get(Key key);
-    public void put(Key key, Value value);
-    public boolean search(Key key);
-    public int size();
-    public boolean isEmpty();
-}
