51ae55f62d357e463161e33804e2c3fe99b1faa3
KH Kim <kaist19@kaist.ac.kr>
2022-04-12 17:59:43 +0900

    update

--- hello.py
+++ hello.py
@@ -1,32 +1,55 @@
-from itertools import permutations
+class Node(object):
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = {}
 
-def quicksort(arr):
-    """
-    arr: list
-    """
-    def partition(arr, left, right):
-        x = arr[right]
-        i = left - 1
-        for j in range(left, right):
-            print(j + 1, right + 1, arr[j] <= x)
-            if arr[j] <= x:
-                i += 1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i+1], arr[right] = arr[right], arr[i+1]
-        return i + 1
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
     
-    def sort(left, right):
-        if right <= left:
-            return
+    def insert(self, string):
+        """
+        insert the string to the trie
+        """
+        current_node = self.head
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.data = string
+    
+    def search(self, string):
+        """
+        string: string to search in the trie
         
-        mid = partition(arr, left, right)
-        sort(left, mid - 1)
-        sort(mid, right)
+        returns True if there is/False otherwise
+        """
+        current_node = self.head
+
+        for char in string:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                return False
+        
+        if current_node.data:
+            return True
+        else:
+            return False
     
-    sort(0, len(arr) - 1)
 
 # Test
-for a, b, c, d in permutations([1, 2, 3, 4]):
-    print(a, b, c, d)
-    arr = [a, b, c, d]
-    quicksort(arr)
+n, m = map(int, input().split())
+trie = Trie()
+for _ in range(n):
+    trie.insert(input().rstrip())
+
+cnt = 0
+for _ in range(m):
+    if trie.search(input().rstrip()):
+        cnt += 1
+
+print(cnt)
