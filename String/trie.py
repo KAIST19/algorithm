83e15396bb3349303d0872d1ccd6e80b0ed75e2e
KH Kim <kaist19@kaist.ac.kr>
2022-04-12 17:59:49 +0900

    update

---
+++ String/trie.py
@@ -0,0 +1,77 @@
+class Node(object):
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = {}
+
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+    
+    def insert(self, string):
+        """
+        insert the string to the trie
+        """
+        current_node = self.head
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.data = string
+    
+    def search(self, string):
+        """
+        string: string to search in the trie
+        
+        returns True if there is/False otherwise
+        """
+        current_node = self.head
+
+        for char in string:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                return False
+        
+        if current_node.data:
+            return True
+        else:
+            return False
+    
+    def starts_with(self, prefix):
+        """
+        prefix: string to search in the trie
+        returns a list of strings that starts with the prefix
+        """
+        current_node = self.head
+        words = []
+        
+        for p in prefix:
+            if p in current_node.children:
+                current_node = current_node.children[p]
+            else:
+                return None
+        
+        current_node = [current_node]
+        next_node = []
+        while True:
+            for node in current_node:
+                if node.data:
+                    words.append(node.data)
+                next_node.extend(list(node.children.values()))
+            if len(next_node) != 0:
+                current_node = next_node
+                next_node = []
+            else:
+                break
+        
+        return words
+            
+
+# Test
+trie = Trie()
+trie.insert("Hello")
+print(trie.search("Hello"))
+print(trie.starts_with("H"))
---
+++ geometry/convex_hull.py
@@ -0,0 +1,26 @@
+def cross(a, b, c):
+    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+
+
+def convex_hull(points):
+    """
+    points: list
+        1) contains (x, y)
+    
+    returns a list of points that form the convex hull
+    """
+    points = sorted(set(points))
+    n = len(points)
+    if n <= 1:
+        return points
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+    return lower[:-1] + upper[:-1]
